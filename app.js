61842d169d4d89c637e2d608d4d92db10c376a16
Prasanna Sridharan <prasannas090801@gmail.com>
2022-01-18 18:42:42 +0530

    Added express.json() middleware

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const cors = require('cors')
 
 //Middlewares
 app.use(cors())
+app.use(express.json());
 app.use(morgan('dev'))                                   //If you want logging
 app.use(express.static(path.join(__dirname, 'public')))  //If you want to serve static files
 
